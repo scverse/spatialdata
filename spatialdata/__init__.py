ecaad9856048d8dc89072f5fda3c9b375acc6d5d
Isaac Virshup <ivirshup@gmail.com>
2022-06-25 14:08:55 +0200

    Initial structure

--- spatialdata/__init__.py
+++ spatialdata/__init__.py
@@ -1,7 +1,5 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
-
-__all__ = ["pl", "pp", "tl"]
-
 __version__ = version("spatialdata")
+
+from ._core import SpatialData
--- spatialdata/pl/basic.py
+++
@@ -1,7 +0,0 @@
-from anndata import AnnData
-
-
-def basic_plot(adata: AnnData) -> int:
-    """Generate a basic plot for an AnnData object."""
-    print("Import matplotlib and implement a plotting function here.")
-    return 0
