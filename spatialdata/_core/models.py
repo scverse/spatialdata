e8b5268c96b223e40eaec1b2cdd9051507a164a4
giovp <giov.pll@gmail.com>
2022-10-12 11:53:08 +0200

    init

---
+++ spatialdata/_core/models.py
@@ -0,0 +1,51 @@
+"""This file contains models and schema for SpatialData"""
+
+from dataclasses import dataclass
+from typing import Literal, Tuple
+
+import numpy as np
+from xarray_dataclasses import AsDataArray, Data
+from xarray_schema import DataArraySchema
+
+Labels_t = np.int_
+Image_t = np.float_
+Table_X_t = np.float_
+
+X, X_t = "x", Literal["x"]
+Y, Y_t = "y", Literal["y"]
+C, C_t = "c", Literal["c"]
+Z, Z_t = "z", Literal["z"]
+
+Labels2D_s = DataArraySchema(dtype=Labels_t, dims=(Y, X))
+Image2D_s = DataArraySchema(dtype=Image_t, dims=(C, Y, X))
+
+Labels3D_s = DataArraySchema(dtype=Labels_t, dims=(Z, Y, X))
+Image3D_s = DataArraySchema(dtype=Image_t, dims=(Z, C, Y, X))
+
+
+@dataclass
+class Labels2D(AsDataArray):
+    """2D Label as DataArray."""
+
+    data: Data[Tuple[Y_t, X_t], Labels_t]
+
+
+@dataclass
+class Image2D(AsDataArray):
+    """2D Image as DataArray."""
+
+    data: Data[Tuple[C_t, Y_t, X_t], Image_t]
+
+
+@dataclass
+class Labels3D(AsDataArray):
+    """3D Label as DataArray."""
+
+    data: Data[Tuple[Z_t, Y_t, X_t], Labels_t]
+
+
+@dataclass
+class Image3D(AsDataArray):
+    """3D Image as DataArray."""
+
+    data: Data[Tuple[Z_t, C_t, Y_t, X_t], Labels_t]
