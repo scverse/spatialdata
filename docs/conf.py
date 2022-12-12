32dd48020c2da6ddb3831d37f3a928f0c007b5bb
giovp <giov.pll@gmail.com>
2022-12-12 13:35:35 +0100

    suppress warning

--- docs/conf.py
+++ docs/conf.py
@@ -97,12 +97,7 @@ intersphinx_mapping = {
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
-    "**.ipynb_checkpoints",
-]
+exclude_patterns = ["_build", "Thumbs.db", "**.ipynb_checkpoints", "generated/spatialdata.PolygonsModel.rst"]
 # Ignore warnings.
 nitpicky = False  # TODO: solve upstream.
 # nitpick_ignore = [
