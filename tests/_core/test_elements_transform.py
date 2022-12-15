fixed bug on physical units, added coordinate_systems attribute in spatialdata

tests/_core/test_elements_transform.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 from spatialdata import SpatialData
-from spatialdata._core.coordinate_system import CoordinateSystem
+from spatialdata._core.coordinate_system import CoordinateSystem, Axis
 from spatialdata._core.core_utils import get_transform, set_transform
 from spatialdata._core.transformations import Scale
 from tests._core.conftest import xy_cs
@@ -44,3 +44,21 @@ class TestElementsTransform:
         shapes.write(tmpdir)
         SpatialData.read(tmpdir)
         assert get_transform(shapes.shapes["shapes_0"]) == transform
+
+    def test_coordinate_systems(self, points: SpatialData) -> None:
+        ct = Scale(np.array([1, 2, 3]))
+        ct.input_coordinate_system = xy_cs
+        ct.output_coordinate_system = CoordinateSystem(name="test", axes=[Axis(name="c", type="channel")])
+        points.points['points_0_1'] = set_transform(points.points['points_0_1'], ct)
+        assert list(points.coordinate_systems.keys()) == ['cyx', 'test']
+
+    def test_physical_units(self, tmp_path: str, points: SpatialData) -> None:
+        tmpdir = Path(tmp_path) / "tmp.zarr"
+        ct = Scale(np.array([1, 2, 3]))
+        ct.input_coordinate_system = xy_cs
+        ct.output_coordinate_system = CoordinateSystem(name="test", axes=[Axis(name="x", type="space",
+                                                                               unit='micrometers')])
+        points.points['points_0_1'] = set_transform(points.points['points_0_1'], ct)
+        points.write(tmpdir)
+        new_sdata = SpatialData.read(tmpdir)
+        assert new_sdata.coordinate_systems['test']._axes[0].unit == 'micrometers'

tests/conftest.py
@@ -1,4 +1,4 @@
-from typing import Mapping, Optional, Sequence, Union
+from typing import Dict, Optional, Sequence, Union
 
 import numpy as np
 import pandas as pd
@@ -8,6 +8,10 @@ from anndata import AnnData
 from geopandas import GeoDataFrame
 from numpy.random import default_rng
 from shapely.geometry import MultiPolygon, Polygon
+from spatial_image import SpatialImage
+from multiscale_spatial_image import MultiscaleSpatialImage
+from geopandas import GeoDataFrame
+import pyarrow as pa
 
 from spatialdata import SpatialData
 from spatialdata._core.models import (
@@ -114,7 +118,7 @@ def sdata(request) -> SpatialData:
     return s
 
 
-def _get_images() -> Mapping[str, Sequence[NDArray]]:
+def _get_images() -> Dict[str, Union[SpatialImage, MultiscaleSpatialImage]]:
     out = {}
     dims_2d = ("c", "y", "x")
 
@@ -149,7 +153,7 @@ def _get_images() -> Mapping[str, Sequence[NDArray]]:
     return out
 
 
-def _get_labels() -> Mapping[str, Sequence[NDArray]]:
+def _get_labels() -> Dict[str, Union[SpatialImage, MultiscaleSpatialImage]]:
     out = {}
     dims_2d = ("y", "x")
     dims_3d = ("z", "y", "x")
@@ -186,7 +190,7 @@ def _get_labels() -> Mapping[str, Sequence[NDArray]]:
     return out
 
 
-def _get_polygons() -> Mapping[str, Sequence[NDArray]]:
+def _get_polygons() -> Dict[str, GeoDataFrame]:
     # TODO: add polygons from geojson and from ragged arrays since now only the GeoDataFrame initializer is tested.
     out = {}
     poly = GeoDataFrame(
@@ -227,7 +231,7 @@ def _get_polygons() -> Mapping[str, Sequence[NDArray]]:
     return out
 
 
-def _get_shapes() -> Mapping[str, Sequence[NDArray]]:
+def _get_shapes() -> Dict[str, AnnData]:
     out = {}
     arr = RNG.normal(size=(100, 2))
     out["shapes_0"] = ShapesModel.parse(arr, shape_type="Square", shape_size=3)
@@ -236,7 +240,7 @@ def _get_shapes() -> Mapping[str, Sequence[NDArray]]:
     return out
 
 
-def _get_points() -> Mapping[str, Sequence[NDArray]]:
+def _get_points() -> Dict[str, pa.Table]:
     name = "points"
     var_names = [np.arange(3), ["genex", "geney"]]
 
@@ -255,7 +259,7 @@ def _get_points() -> Mapping[str, Sequence[NDArray]]:
 
 
 def _get_table(
-    region: Optional[Union[str, Sequence[str]]] = None,
+    region: Optional[AnnData] = None,
     region_key: Optional[str] = None,
     instance_key: Optional[str] = None,
 ) -> AnnData: