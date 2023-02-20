42706ffa974e13d208037c6c2b6fd96e6fcd9f8d
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2023-02-20 16:08:12 +0100

    add shapes to dataset

--- spatialdata/_dl/datasets.py
+++ spatialdata/_dl/datasets.py
@@ -1,9 +1,11 @@
 from typing import Callable, Optional
 
+import numpy as np
 from torch.utils.data import Dataset
 
 from spatialdata import SpatialData
 from spatialdata._core._spatial_query import BoundingBoxRequest
+from spatialdata._core.core_utils import get_dims
 from spatialdata._types import ArrayLike
 
 
@@ -11,23 +13,18 @@ class SpotCropDataset(Dataset):
     def __init__(
         self,
         sdata: SpatialData,
-        coordinates: ArrayLike,
-        radius: int = 5,
+        spots_element_key: str,
         transform: Optional[Callable[[SpatialData], SpatialData]] = None,
     ):
         self.sdata = sdata
+        self.spots_element_key = spots_element_key
         self.transform = transform
 
-        self.coordinates = coordinates
-        self.radius = radius
+        self.min_coordinates, self.max_coordinates, self.spots_dims = self._get_bounding_box_coordinates()
+        self.n_spots = len(self.min_coordinates)
 
-    def _get_bounding_box_coordinates(self, spot_index: int) -> tuple[ArrayLike, ArrayLike]:
+    def _get_bounding_box_coordinates(self) -> tuple[ArrayLike, ArrayLike, tuple[str, ...]]:
         """Get the coordinates for the corners of the bounding box of that encompasses a given spot.
-
-        Parameters
-        ----------
-        spot_index
-            The row index of the spot.
 
         Returns
         -------
@@ -36,19 +33,28 @@ class SpotCropDataset(Dataset):
         max_coordinate
             The maximum coordinate of the bounding box.
         """
-        centroid = self.coordinates[spot_index]
-        min_coordinate = centroid - self.radius
-        max_coordinate = centroid + self.radius
+        spots_element = self.sdata.shapes[self.spots_element_key]
+        spots_dims = get_dims(spots_element)
 
-        return min_coordinate, max_coordinate
+        centroids = []
+        for dim_name in spots_dims:
+            centroids.append(getattr(spots_element["geometry"], dim_name).to_numpy())
+        centroids_array = np.column_stack(centroids)
+        radius = np.expand_dims(spots_element["radius"].to_numpy(), axis=1)
+
+        min_coordinates = (centroids_array - radius).astype(int)
+        max_coordinates = (centroids_array + radius).astype(int)
+
+        return min_coordinates, max_coordinates, spots_dims
 
     def __len__(self) -> int:
-        return len(self.coordinates)
+        return self.n_spots
 
     def __getitem__(self, idx: int) -> SpatialData:
-        min_coordinate, max_coordinate = self._get_bounding_box_coordinates(spot_index=idx)
+        min_coordinate = self.min_coordinates[idx]
+        max_coordinate = self.max_coordinates[idx]
 
-        request = BoundingBoxRequest(min_coordinate=min_coordinate, max_coordinate=max_coordinate, axes=("y", "x"))
+        request = BoundingBoxRequest(min_coordinate=min_coordinate, max_coordinate=max_coordinate, axes=self.spots_dims)
         sdata_item = self.sdata.query.bounding_box(request=request)
 
         if self.transform is not None:
