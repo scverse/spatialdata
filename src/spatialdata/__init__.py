43297261d1ea3c398bf0a922fc8907b7d760a6c9
Luca Marconato <2664412+LucaMarconato@users.noreply.github.com>
2023-03-15 23:51:42 +0100

    added module dataloader

--- src/spatialdata/__init__.py
+++ src/spatialdata/__init__.py
@@ -1,5 +1,4 @@
 from importlib.metadata import version
-from typing import Union
 
 __version__ = version("spatialdata")
 
@@ -11,29 +10,22 @@ _check_geopandas_using_shapely()
 
 
 __all__ = [
-    "concatenate",
-    "rasterize",
-    "bounding_box_query",
-    "transform",
-    "SpatialData",
     "element_utils",
     "models",
     "transformations",
+    "dataloader",
+    "concatenate",
+    "rasterize",
+    "transform",
+    "bounding_box_query",
+    "SpatialData",
     "read_zarr",
 ]
 
-from spatialdata import element_utils, models, transformations
+from spatialdata import dataloader, element_utils, models, transformations
 from spatialdata._core.concatenate import concatenate
 from spatialdata._core.operations.rasterize import rasterize
 from spatialdata._core.operations.transform import transform
 from spatialdata._core.query.spatial_query import bounding_box_query
 from spatialdata._core.spatialdata import SpatialData
-
-from ._io.io_zarr import read_zarr
-
-try:
-    from spatialdata._dataloader.datasets import ImageTilesDataset
-except ImportError as e:
-    _error: Union[str, None] = str(e)
-else:
-    _error = None
+from spatialdata._io.io_zarr import read_zarr
