007fd86ec65bb22e869f77619e4c5c41fe5405bd
Luca Marconato <2664412+LucaMarconato@users.noreply.github.com>
2023-05-16 11:31:48 +0200

    fix query with buffered circles

--- src/spatialdata/_core/query/_utils.py
+++ src/spatialdata/_core/query/_utils.py
@@ -7,7 +7,10 @@ def circles_to_polygons(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
 
     values_geotypes = list(df.geom_type.unique())
     if values_geotypes == ["Point"]:
-        df = df.set_geometry(df.geometry.buffer(df[ShapesModel.RADIUS_KEY]))
+        buffered_df = df.set_geometry(df.geometry.buffer(df[ShapesModel.RADIUS_KEY]))
+        # TODO replace with a function to copy the metadata (the parser could also do this): https://github.com/scverse/spatialdata/issues/258
+        buffered_df.attrs[ShapesModel.TRANSFORM_KEY] = df.attrs[ShapesModel.TRANSFORM_KEY]
+        return buffered_df
     elif "Point" in values_geotypes:
         raise TypeError("Geometry contained shapes and polygons.")
     return df
