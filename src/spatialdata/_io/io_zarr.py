b3fa292c0a1abcea835c5d7f08f1786836930345
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2023-05-23 15:08:36 +0200

    ignore hidden files

--- src/spatialdata/_io/io_zarr.py
+++ src/spatialdata/_io/io_zarr.py
@@ -33,19 +33,26 @@ def read_zarr(store: Union[str, Path, zarr.Group]) -> SpatialData:
     # read multiscale images
     if 'images' in f:
         images_store = f["images"]
+        count = 0
         for k in images_store:
+            if Path(k).name.startswith("."):
+                continue
             f_elem = images_store[k]
             f_elem_store = Path(images_store._store.path) / f_elem.path
             logger.debug(f"Store path for multiscale {f_elem}")
             element = _read_multiscale(f_elem_store, raster_type="image")
             print(element)
             images[k] = element
+            count += 1
+        logger.debug(f"Found {count} images in {f}")
 
     # read multiscale labels
     with ome_zarr_logger(logging.ERROR):
         if 'labels' in f:
             labels_store = f["labels"]
             for k in labels_store:
+                if Path(k).name.startswith("."):
+                    continue
                 f_elem = labels_store[k]
                 f_elem_store = Path(labels_store._store.path) / f_elem.path
                 logger.debug(f"Store path for multiscale {f_elem}")
@@ -56,6 +63,8 @@ def read_zarr(store: Union[str, Path, zarr.Group]) -> SpatialData:
         points_store = f["points"]
         for k in points_store:
             f_elem = points_store[k]
+            if Path(k).name.startswith("."):
+                continue
             f_elem_store = Path(points_store._store.path) / f_elem.path
             logger.debug(f"Store path for points {f_elem_store}")
             points[k] = _read_points(f_elem_store)
@@ -63,6 +72,8 @@ def read_zarr(store: Union[str, Path, zarr.Group]) -> SpatialData:
     if 'shapes' in f:
         shapes_store = f["shapes"]
         for k in shapes_store:
+            if Path(k).name.startswith("."):
+                continue
             f_elem = shapes_store[k]
             f_elem_store = Path(shapes_store._store.path) / f_elem.path
             shapes[k] = _read_shapes(f_elem_store)
@@ -70,6 +81,8 @@ def read_zarr(store: Union[str, Path, zarr.Group]) -> SpatialData:
     if 'table' in f:
         table_store = f["table"]
         for k in table_store:
+            if Path(k).name.startswith("."):
+                continue
             f_elem = table_store[k]
             f_elem_store = Path(table_store._store.path) / f_elem.path
             table = read_anndata_zarr(f_elem_store)
