9aa752593dc284ceb2db5ed329b22a455875428f
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2023-07-14 19:00:39 +0200

    Support for consolidated remote zarr (#278)
    Co-authored-by: Luca Marconato <m.lucalmer@gmail.com>

--- src/spatialdata/__main__.py
+++ src/spatialdata/__main__.py
@@ -3,39 +3,48 @@ The CLI Interaction module.
 
 This module provides command line interface (CLI) interactions for the SpatialData library, allowing users to perform
 various operations through a terminal. Currently, it implements the "peek" function, which allows users to inspect
-the contents of a SpatialData .zarr file. Additional CLI functionalities will be implemented in the future.
+the contents of a SpatialData .zarr dataset. Additional CLI functionalities will be implemented in the future.
 """
-import os
+from typing import Literal
 
 import click
 
 
-@click.command(help="Peek inside the SpatialData .zarr file")
+@click.command(help="Peek inside the SpatialData .zarr dataset")
 @click.argument("path", default=False, type=str)
-def peek(path: str) -> None:
+@click.argument("selection", type=click.Choice(["images", "labels", "points", "shapes", "table"]), nargs=-1)
+def peek(path: str, selection: tuple[Literal["images", "labels", "points", "shapes", "table"]]) -> None:
     """
-    Peek inside the SpatialData .zarr file.
+    Peek inside the SpatialData .zarr dataset.
 
-    This function takes a path to a .zarr file, checks if it is a valid directory, and then reads and prints
-    its contents using the SpatialData library.
+    This function takes a path to a local or remote .zarr dataset, reads and prints
+    its contents using the SpatialData library. If any ValueError is raised, it is caught and printed to the
+    terminal along with a help message.
 
     Parameters
     ----------
     path
-        The path to the .zarr file to be inspected.
+        The path to the .zarr dataset to be inspected.
+    selection
+        Optional, a list of keys (among images, labels, points, shapes, table) to load only a subset of the dataset.
+        Example: `python -m spatialdata peek data.zarr images labels`
     """
-    if not os.path.isdir(path):
+    import spatialdata as sd
+
+    try:
+        sdata = sd.SpatialData.read(path, selection=selection)
+        print(sdata)  # noqa: T201
+    except ValueError as e:
+        # checking if a valid path was provided is difficult given the various ways in which
+        # a possibly remote path and storage access options can be specified
+        # so we just catch the ValueError and print a help message
+        print(e)  # noqa: T201
         print(  # noqa: T201
             f"Error: .zarr storage not found at {path}. Please specify a valid OME-NGFF spatial data (.zarr) file. "
-            "Example "
-            '"python -m '
-            'spatialdata peek data.zarr"'
+            "Examples "
+            '"python -m spatialdata peek data.zarr"'
+            '"python -m spatialdata peek https://remote/.../data.zarr labels table"'
         )
-    else:
-        import spatialdata as sd
-
-        sdata = sd.SpatialData.read(path)
-        print(sdata)  # noqa: T201
 
 
 @click.group()
