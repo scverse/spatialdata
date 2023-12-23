52c439e46ed8f7a076433376a6e285da34e0a53c
wmv_hpomen <w-mv@hotmail.com>
2023-12-23 10:45:40 +0100

    add utility function

---
+++ src/spatialdata/_core/_utils.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from spatialdata import SpatialData
+
+
+def find_common_keys(sdatas: list[SpatialData]) -> set[str]:
+    """
+    Find table keys present in more than one SpatialData object.
+
+    Parameters
+    ----------
+    sdatas
+        A list of SpatialData objects.
+
+    Returns
+    -------
+    A set of common keys that are present in the tables of more than one SpatialData object.
+    """
+    common_keys = set(sdatas[0].tables.keys())
+
+    for sdata in sdatas[1:]:
+        common_keys.intersection_update(sdata.tables.keys())
+
+    return common_keys
