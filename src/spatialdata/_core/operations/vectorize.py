274fd1dee9a6fc078c545b4a70e7f04d3cbd5830
Luca Marconato <m.lucalmer@gmail.com>
2024-03-01 00:22:06 +0100

    added vectorize; still no tests

--- src/spatialdata/_core/operations/vectorize.py
+++ src/spatialdata/_core/operations/vectorize.py
@@ -1,54 +1,32 @@
-from functools import singledispatch
-from typing import Optional, Union
-from warnings import warn
+from __future__ import annotations
 
-import dask_image.ndinterp
+from functools import singledispatch
+
 import numpy as np
 import pandas as pd
-from dask.array.core import Array as DaskArray
-from dask.dataframe.core import DataFrame as DaskDataFrame
 from geopandas import GeoDataFrame
 from multiscale_spatial_image import MultiscaleSpatialImage
+from shapely import MultiPolygon, Point, Polygon
 from spatial_image import SpatialImage
-from xarray import DataArray
+
 from spatialdata._core.centroids import get_centroids
 from spatialdata._core.operations.aggregate import aggregate
-
-from spatialdata._core.spatialdata import SpatialData
-from spatialdata._types import ArrayLike
-from spatialdata._utils import Number, _parse_list_into_array
 from spatialdata.models import (
     Image2DModel,
     Image3DModel,
     ShapesModel,
-    Labels2DModel,
-    Labels3DModel,
     SpatialElement,
     get_axes_names,
-    get_model,
-)
-from shapely import Polygon, MultiPolygon, Point
-from spatialdata.models._utils import get_spatial_axes
-from spatialdata.transformations._utils import _get_scale, compute_coordinates
-from spatialdata.transformations.operations import (
-    get_transformation,
-    remove_transformation,
-    set_transformation,
+    get_table_keys,
 )
 from spatialdata.transformations.transformations import (
-    Affine,
-    BaseTransformation,
-    Scale,
-    Sequence,
-    Translation,
     Identity,
-    _get_affine_for_element,
 )
 
 
 @singledispatch
 def to_circles(
-    data: Union[SpatialElement],
+    data: SpatialElement,
     target_coordinate_system: str,
 ) -> GeoDataFrame:
     """
@@ -73,6 +51,7 @@ def to_circles(
     """
     raise RuntimeError("Unsupported type: {type(data)}")
 
+
 @to_circles.register(SpatialImage)
 @to_circles.register(MultiscaleSpatialImage)
 def _(
@@ -86,12 +65,9 @@ def _(
         shape = element.shape
 
     axes = get_axes_names(element)
-    if "z" in axes:
-        model = Image3DModel
-    else:
-        model = Image2DModel
+    model = Image3DModel if "z" in axes else Image2DModel
     ones = model.parse(np.ones((1,) + shape), dims=("c",) + axes)
-    aggregated = aggregate(values=ones, by=element, agg_func="sum").table
+    aggregated = aggregate(values=ones, by=element, agg_func="sum")["table"]
     areas = aggregated.X.todense().A1.reshape(-1)
     aobs = aggregated.obs
     aobs["areas"] = areas
@@ -102,12 +78,13 @@ def _(
     if 0 in centroids.index:
         centroids = centroids.drop(index=0)
     centroids.index = centroids.index
+    _, _, instance_key = get_table_keys(aobs)
     aobs.index = aobs[instance_key]
     aobs.index.name = None
     assert len(aobs) == len(centroids)
     obs = pd.merge(aobs, centroids, left_index=True, right_index=True, how="inner")
     assert len(obs) == len(centroids)
-
+    return _make_circles(element, obs, target_coordinate_system)
 
 
 @to_circles.register(GeoDataFrame)
@@ -117,14 +94,19 @@ def _(
 ) -> GeoDataFrame:
     if isinstance(element.geometry.iloc[0], (Polygon, MultiPolygon)):
         radius = np.sqrt(element.geometry.area / np.pi)
-            centroids = get_centroids(element, coordinate_system=coordinate_system).compute()
-            obs = pd.DataFrame({"radius": radius})
-            obs = pd.merge(obs, centroids, left_index=True, right_index=True, how="inner")
-        return _make_circles(element, obs, coordinate_system)
-    else:
-        return sdata[region_name]
+        centroids = get_centroids(element, coordinate_system=target_coordinate_system).compute()
+        obs = pd.DataFrame({"radius": radius})
+        obs = pd.merge(obs, centroids, left_index=True, right_index=True, how="inner")
+        return _make_circles(element, obs, target_coordinate_system)
+    assert isinstance(element.geometry.iloc[0], Point), (
+        f"Unsupported geometry type: " f"{type(element.geometry.iloc[0])}"
+    )
+    return element
 
-def _make_circles(element: SpatialImage | MultiscaleSpatialImage | GeoDataFrame, obs: pd.DataFrame, coordinate_system: str) -> GeoDataFrame:
+
+def _make_circles(
+    element: SpatialImage | MultiscaleSpatialImage | GeoDataFrame, obs: pd.DataFrame, coordinate_system: str
+) -> GeoDataFrame:
     spatial_axes = sorted(get_axes_names(element))
     centroids = obs[spatial_axes].values
     return ShapesModel.parse(
@@ -135,11 +117,12 @@ def _make_circles(element: SpatialImage | MultiscaleSpatialImage | GeoDataFrame,
         transformations={coordinate_system: Identity()},
     )
 
+
 # TODO: depending of the implementation, add a parameter to control the degree of approximation of the constructed
 # polygons/multipolygons
 @singledispatch
 def to_polygons(
-    data: Union[SpatialElement],
+    data: SpatialElement,
     target_coordinate_system: str,
 ) -> GeoDataFrame:
     """
