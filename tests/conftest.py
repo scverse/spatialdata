9aa98e1eaa5ed1c245bf57f2e9c03ce626702eaa
Luca Marconato <m.lucalmer@gmail.com>
2024-03-13 19:25:20 +0100

    fixed missings seeds for default_rng()

--- tests/conftest.py
+++ tests/conftest.py
@@ -295,7 +295,7 @@ def _get_table(
 
 
 def _get_new_table(spatial_element: None | str | Sequence[str], instance_id: None | Sequence[Any]) -> AnnData:
-    adata = AnnData(np.random.default_rng().random(10, 20000))
+    adata = AnnData(np.random.default_rng(seed=0).random(10, 20000))
     return TableModel.parse(adata=adata, spatial_element=spatial_element, instance_id=instance_id)
 
 
