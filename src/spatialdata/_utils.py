3add21aa74ec0dd43eafdfb37e9a689c07729ca7
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2024-11-11 11:23:53 +0100

    move skip_non_dimension_nodes -> multiscale-spatial-image v2.0.1

--- src/spatialdata/_utils.py
+++ src/spatialdata/_utils.py
@@ -311,22 +311,3 @@ def _error_message_add_element() -> None:
         "write_labels(), write_points(), write_shapes() and write_table(). We are going to make these calls more "
         "ergonomic in a follow up PR."
     )
-
-
-def skip_non_dimension_nodes(func: Callable[[Dataset], Dataset]) -> Callable[[Dataset], Dataset]:
-    """Skip nodes in Datatree that do not contain dimensions.
-
-    This function implements the workaround of https://github.com/pydata/xarray/issues/9693. In particular,
-    we need this because of our DataTree representing multiscale image having a root node that does not have
-    dimensions. Several functions need to be mapped over the datasets in the datatree that depend on having
-    dimensions, e.g. a transpose.
-    """
-
-    @functools.wraps(func)
-    def _func(ds: Dataset, *args: Any, **kwargs: Any) -> Dataset:
-        # check if dimensions are present otherwise return verbatim
-        if len(ds.dims) == 0:
-            return ds
-        return func(ds, *args, **kwargs)
-
-    return _func
