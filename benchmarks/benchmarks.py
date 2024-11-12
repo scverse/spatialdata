44cb698fe1a0e029a7a2a746f434f39b1eadb081
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2024-11-12 14:50:18 +0100

    add basic benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -2,35 +2,11 @@
 # See "Writing benchmarks" in the asv docs for more information.
 import spatialdata as sd
 
-# class TimeSuite:
-#     """
-#     An example benchmark that times the performance of various kinds
-#     of iterating over dictionaries in Python.
-#     """
-#     def setup(self):
-#         self.d = {}
-#         for x in range(500):
-#             self.d[x] = None
 
-#     def time_keys(self):
-#         for key in self.d.keys():
-#             pass
+class MemorySpatialData:
+    # TODO: see what the memory overhead is e.g. Python interpreter...
+    """Calculate the peak memory usage is for artificial datasets with increasing channels."""
 
-#     def time_values(self):
-#         for value in self.d.values():
-#             pass
-
-#     def time_range(self):
-#         d = self.d
-#         for key in range(500):
-#             d[key]
-
-# class MemSuite:
-#     def mem_list(self):
-#         sdata: sd.SpatialData = sd.datasets.blobs()
-#         return sdata
-
-class SpatialBlobsSuite:
     def peakmem_list(self):
         sdata: sd.SpatialData = sd.datasets.blobs(n_channels=1)
         return sdata
@@ -41,13 +17,15 @@ class SpatialBlobsSuite:
 
 
 def timeraw_import_inspect():
+    """Time the import of the spatialdata module."""
     return """
     import spatialdata
     """
 
-class SpatialDataLoading:
+class TimeMapRaster:
+    """Time the."""
 
-    params = [100, 200, 300]
+    params = [100, 1000]
     param_names = ["length"]
 
     def setup(self, length):
@@ -59,3 +37,30 @@ class SpatialDataLoading:
     def time_map_blocks(self, _):
         sd.map_raster(self.sdata["blobs_image"], lambda x: x+1)
 
+class TimeQueries:
+
+    params = ([100, 1000], [True, False])
+    param_names = ["length", "filter_table"]
+
+    def setup(self, length, filter_table):
+        import shapely
+        self.sdata = sd.datasets.blobs(length=length)
+        self.polygon = shapely.box(0, 0, 100, 100)
+
+
+    def teardown(self, length, filter_table):
+        del self.sdata
+
+    def time_query_bounding_box(self, length, filter_table):
+        self.sdata.query.bounding_box(
+            axes=["x", "y"],
+            min_coordinate=[0, 0],
+            max_coordinate=[100, 100],
+            target_coordinate_system="global",
+            filter_table=filter_table,
+        )
+
+    def time_query_polygon_box(self, length, filter_table):
+        sd.polygon_query(self.sdata, self.polygon, target_coordinate_system="global",
+                         filter_table=filter_table,
+        )
