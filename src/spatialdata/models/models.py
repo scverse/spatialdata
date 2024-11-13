8ff16ef2a11215ba94f6193643da8dcbb1f7b91a
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2024-11-13 01:17:10 +0100

    add c_coords docstring specific for images

--- src/spatialdata/models/models.py
+++ src/spatialdata/models/models.py
@@ -98,7 +98,7 @@ class RasterSchema(DataArraySchema):
         chunks: Chunks_t | None = None,
         **kwargs: Any,
     ) -> DataArray | DataTree:
-        """
+        r"""
         Validate (or parse) raster data.
 
         Parameters
@@ -111,6 +111,8 @@ class RasterSchema(DataArraySchema):
             Dimensions of the data (e.g. ['c', 'y', 'x'] for 2D image data). If the data is a :class:`xarray.DataArray`,
             the dimensions can also be inferred from the data. If the dimensions are not in the order (c)(z)yx, the data
             will be transposed to match the order.
+        {("c_coords : str | list[str] | None\n            Set channel coordinates for image data. This is only"
+        "applicable for image data; labels data do not have channels.\n" if cls.INCLUDE_C_COORDS else "")}
         transformations
             Dictionary of transformations to apply to the data. The key is the name of the target coordinate system,
             the value is the transformation to apply. By default, a single `Identity` transformation mapping to the
@@ -317,6 +319,7 @@ class Image2DModel(RasterSchema):
     attrs = AttrsSchema({"transform": Transform_s})
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.INCLUDE_C_COORDS = True
         super().__init__(
             dims=self.dims,
             array_type=self.array_type,
@@ -332,6 +335,7 @@ class Image3DModel(RasterSchema):
     attrs = AttrsSchema({"transform": Transform_s})
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.INCLUDE_C_COORDS = True
         super().__init__(
             dims=self.dims,
             array_type=self.array_type,
