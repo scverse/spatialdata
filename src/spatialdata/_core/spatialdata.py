1a205bc81f2fe6c71e4677ad7dc46081f07de874
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2024-12-10 11:43:45 -0500

    transposing df

--- src/spatialdata/_core/spatialdata.py
+++ src/spatialdata/_core/spatialdata.py
@@ -1709,7 +1709,7 @@ class SpatialData:
 
         if return_as == "df":
             try:
-                return pd.DataFrame([data])
+                return pd.DataFrame([data]).T
             except Exception as e:
                 raise ValueError(f"Failed to convert data to DataFrame: {e}") from e
 
