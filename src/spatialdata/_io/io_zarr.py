c0ffb1c13118f23d3e14b7aa4d6510cb07e2ee49
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2025-02-01 10:14:13 +0100

    remove f_store_path, support remote raster types fully and keep local support for other elements

--- src/spatialdata/_io/io_zarr.py
+++ src/spatialdata/_io/io_zarr.py
@@ -1,13 +1,15 @@
 import logging
-import os
 import warnings
-from pathlib import Path
 
 import zarr
+import zarr.storage
 from anndata import AnnData
+from dask.dataframe import DataFrame as DaskDataFrame
+from geopandas import GeoDataFrame
+from xarray import DataArray, DataTree
 
 from spatialdata._core.spatialdata import SpatialData
-from spatialdata._io._utils import ome_zarr_logger
+from spatialdata._io._utils import StoreLike, _create_upath, _open_zarr_store, ome_zarr_logger
 from spatialdata._io.io_points import _read_points
 from spatialdata._io.io_raster import _read_multiscale
 from spatialdata._io.io_shapes import _read_shapes
@@ -15,28 +17,51 @@ from spatialdata._io.io_table import _read_table
 from spatialdata._logging import logger
 
 
-def _open_zarr_store(store: str | Path | zarr.Group) -> tuple[zarr.Group, str]:
-    """
-    Open a zarr store (on-disk or remote) and return the zarr.Group object and the path to the store.
+def read_image_element(path: StoreLike) -> DataArray | DataTree:
+    """Read a single image element from a store location.
 
     Parameters
     ----------
-    store
-        Path to the zarr store (on-disk or remote) or a zarr.Group object.
+    path
+        Path to the zarr store.
 
     Returns
     -------
-    A tuple of the zarr.Group object and the path to the store.
+    A DataArray or DataTree object.
     """
-    f = store if isinstance(store, zarr.Group) else zarr.open(store, mode="r")
-    # workaround: .zmetadata is being written as zmetadata (https://github.com/zarr-developers/zarr-python/issues/1121)
-    if isinstance(store, str | Path) and str(store).startswith("http") and len(f) == 0:
-        f = zarr.open_consolidated(store, mode="r", metadata_key="zmetadata")
-    f_store_path = f.store.store.path if isinstance(f.store, zarr.storage.ConsolidatedMetadataStore) else f.store.path
-    return f, f_store_path
+    store = _open_zarr_store(path)
+    return _read_multiscale(store, raster_type="image")
 
 
-def read_zarr(store: str | Path | zarr.Group, selection: None | tuple[str] = None) -> SpatialData:
+def read_labels_element(path: StoreLike) -> DataArray | DataTree:
+    """Read a single image element from a store location.
+
+    Parameters
+    ----------
+    path
+        Path to the zarr store.
+
+    Returns
+    -------
+    A DataArray or DataTree object.
+    """
+    store = _open_zarr_store(path)
+    return _read_multiscale(store, raster_type="labels")
+
+
+def read_points_element() -> DaskDataFrame:
+    pass
+
+
+def read_shapes_element() -> GeoDataFrame:
+    pass
+
+
+def read_table_element() -> AnnData:
+    pass
+
+
+def read_zarr(store_like: StoreLike, selection: None | tuple[str] = None) -> SpatialData:
     """
     Read a SpatialData dataset from a zarr store (on-disk or remote).
 
@@ -53,7 +78,10 @@ def read_zarr(store: str | Path | zarr.Group, selection: None | tuple[str] = Non
     -------
     A SpatialData object.
     """
-    f, f_store_path = _open_zarr_store(store)
+    store = _open_zarr_store(store_like)
+    f = zarr.group(store)
+    # TODO: remove table once deprecated.
+    selector = {"images", "labels", "points", "shapes", "tables", "table"} if not selection else set(selection or [])
 
     images = {}
     labels = {}
@@ -61,21 +89,18 @@ def read_zarr(store: str | Path | zarr.Group, selection: None | tuple[str] = Non
     tables: dict[str, AnnData] = {}
     shapes = {}
 
-    # TODO: remove table once deprecated.
-    selector = {"images", "labels", "points", "shapes", "tables", "table"} if not selection else set(selection or [])
     logger.debug(f"Reading selection {selector}")
 
     # read multiscale images
     if "images" in selector and "images" in f:
-        group = f["images"]
+        group = f.images
         count = 0
         for subgroup_name in group:
-            if Path(subgroup_name).name.startswith("."):
+            if subgroup_name.startswith("."):
                 # skip hidden files like .zgroup or .zmetadata
                 continue
             f_elem = group[subgroup_name]
-            f_elem_store = os.path.join(f_store_path, f_elem.path)
-            element = _read_multiscale(f_elem_store, raster_type="image")
+            element = read_image_element(f_elem)
             images[subgroup_name] = element
             count += 1
         logger.debug(f"Found {count} elements in {group}")
@@ -83,15 +108,14 @@ def read_zarr(store: str | Path | zarr.Group, selection: None | tuple[str] = Non
     # read multiscale labels
     with ome_zarr_logger(logging.ERROR):
         if "labels" in selector and "labels" in f:
-            group = f["labels"]
+            group = f.labels
             count = 0
             for subgroup_name in group:
-                if Path(subgroup_name).name.startswith("."):
+                if subgroup_name.startswith("."):
                     # skip hidden files like .zgroup or .zmetadata
                     continue
                 f_elem = group[subgroup_name]
-                f_elem_store = os.path.join(f_store_path, f_elem.path)
-                labels[subgroup_name] = _read_multiscale(f_elem_store, raster_type="labels")
+                labels[subgroup_name] = read_labels_element(f_elem)
                 count += 1
             logger.debug(f"Found {count} elements in {group}")
 
@@ -100,12 +124,11 @@ def read_zarr(store: str | Path | zarr.Group, selection: None | tuple[str] = Non
         group = f["points"]
         count = 0
         for subgroup_name in group:
-            f_elem = group[subgroup_name]
-            if Path(subgroup_name).name.startswith("."):
+            if subgroup_name.startswith("."):
                 # skip hidden files like .zgroup or .zmetadata
                 continue
-            f_elem_store = os.path.join(f_store_path, f_elem.path)
-            points[subgroup_name] = _read_points(f_elem_store)
+            f_elem = group[subgroup_name]
+            points[subgroup_name] = _read_points(f_elem)
             count += 1
         logger.debug(f"Found {count} elements in {group}")
 
@@ -113,30 +136,26 @@ def read_zarr(store: str | Path | zarr.Group, selection: None | tuple[str] = Non
         group = f["shapes"]
         count = 0
         for subgroup_name in group:
-            if Path(subgroup_name).name.startswith("."):
+            if subgroup_name.startswith("."):
                 # skip hidden files like .zgroup or .zmetadata
                 continue
             f_elem = group[subgroup_name]
-            f_elem_store = os.path.join(f_store_path, f_elem.path)
-            shapes[subgroup_name] = _read_shapes(f_elem_store)
+            shapes[subgroup_name] = _read_shapes(f_elem)
             count += 1
         logger.debug(f"Found {count} elements in {group}")
     if "tables" in selector and "tables" in f:
         group = f["tables"]
-        tables = _read_table(f_store_path, f, group, tables)
+        tables = _read_table(zarr_store_path=str(store_like), group=f, subgroup=group, tables=tables)
 
     if "table" in selector and "table" in f:
         warnings.warn(
-            f"Table group found in zarr store at location {f_store_path}. Please update the zarr store to use tables "
-            f"instead.",
+            f"Table group found in zarr store at location {f}. Please update the zarr store to use tables instead.",
             DeprecationWarning,
             stacklevel=2,
         )
         subgroup_name = "table"
         group = f[subgroup_name]
-        tables = _read_table(f_store_path, f, group, tables)
-
-        logger.debug(f"Found {count} elements in {group}")
+        tables = _read_table(zarr_store_path=store_like, group=f, subgroup=group, tables=tables)
 
     # read attrs metadata
     attrs = f.attrs.asdict()
@@ -155,5 +174,6 @@ def read_zarr(store: str | Path | zarr.Group, selection: None | tuple[str] = Non
         tables=tables,
         attrs=attrs,
     )
-    sdata.path = Path(store)
+    # TODO: create a UPath object from any StoreLike object
+    sdata.path = _create_upath(store_like)
     return sdata
