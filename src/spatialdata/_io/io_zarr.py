28837b1cfbbd656f0c978e93c005f09147f7cc23
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-03-17 16:07:58 +0100

    stay in sync with ome zarr format

--- src/spatialdata/_io/io_zarr.py
+++ src/spatialdata/_io/io_zarr.py
@@ -50,7 +50,9 @@ def read_image_element(path: StoreLike) -> DataArray | DataTree:
     -------
     A DataArray or DataTree object.
     """
-    store = _open_zarr_store(path)
+    # stay in sync with ome v4 format spec:
+    # https://github.com/ome/ome-zarr-py/blob/7d1ae35c97/ome_zarr/format.py#L189-L192
+    store = _open_zarr_store(path, dimension_separator="/", normalize_keys=False)
     return _read_multiscale(store, raster_type="image")
 
 
