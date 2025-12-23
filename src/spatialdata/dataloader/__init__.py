56c9e7e0c4d27321b44aec92cdb73fb3f0523e03
Quentin Blampey <quentin.blampey@gmail.com>
2025-12-23 10:57:33 +0100

    use @Zethson implementation

--- src/spatialdata/dataloader/__init__.py
+++ src/spatialdata/dataloader/__init__.py
@@ -2,25 +2,20 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
+import spatialdata
+
 if TYPE_CHECKING:
-    from spatialdata.dataloader.datasets import ImageTilesDataset as _ImageTilesDataset
+    from spatialdata.dataloader.datasets import ImageTilesDataset
+
+__all__ = [
+    "ImageTilesDataset",
+]
 
 
-class ImageTilesDataset:  # noqa: D101
-    _target_class: type[_ImageTilesDataset] | None = None
+def __getattr__(attr_name: str) -> ImageTilesDataset | Any:
+    if attr_name == "ImageTilesDataset":
+        from spatialdata.dataloader.datasets import ImageTilesDataset
 
-    def __new__(cls, *args: Any, **kwargs: Any) -> _ImageTilesDataset:  # noqa: D102
-        if cls._target_class is None:
-            try:
-                from spatialdata.dataloader.datasets import (
-                    ImageTilesDataset as ActualImageTilesDataset,
-                )
+        return ImageTilesDataset
 
-                cls._target_class = ActualImageTilesDataset
-
-            except ImportError as error:
-                raise ImportError(
-                    "ImageTilesDataset could not be imported. This usually means the 'torch' dependency is missing."
-                ) from error
-
-        return cls._target_class(*args, **kwargs)
+    return getattr(spatialdata.dataloader, attr_name)
