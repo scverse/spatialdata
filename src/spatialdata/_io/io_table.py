2debe5f2559ab61de02d0241a85961ef715a4ec0
Ajayrama Kumaraswamy <ajkumaraswamy@tutamail.com>
2026-08-18 13:30:35 +0200

    AnnData obs/var string to categoricals (#1170)
    * chore: added prek to dev deps
    
    * feat: writing table to zarr using AnnData.write_zarr for table version 2
    
    * chore: added ruff to dev deps
    
    * fix: using internal resolve store function + categorical when writing tables to zarr v2
    
    * fix: test no longer expectes 'nan' after table round trip instead of pd.NA/np.nan
    
    * feat: added hatch env configs for testing version combinations of anndata/pandas
    
    * feat: update hatch config for testing pandas/anndata versions
    
    * feat: added DeprecationWarnings when writing to zarr v2
    
    * fix: if-raise instead of assert
    
    * refac + doc: pyproject.toml
    
    * fix: made version comparison of anndata more robust
    
    * fix: doc string
    
    * fix: made converting table strings to categorical optional + non-default
    
    * feat: documentation strings
    
    * feat: revised tests to work with new write parameter
    
    * fix: removed unnecessary redeclaration of table group
    
    * fix: doc string improvements
    
    * chore: docs touch-up; removed unnecessary .copy()
    
    ---------
    
    Co-authored-by: Luca Marconato <m.lucalmer@gmail.com>
    Co-authored-by: LucaMarconato <2664412+LucaMarconato@users.noreply.github.com>

--- src/spatialdata/_io/io_table.py
+++ src/spatialdata/_io/io_table.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import warnings
+from importlib.metadata import version
 from pathlib import Path
 
 import numpy as np
@@ -8,7 +10,10 @@ from anndata import AnnData
 from anndata import read_zarr as read_anndata_zarr
 from anndata._io.specs import write_elem as write_adata
 from ome_zarr.format import Format
+from packaging.version import Version
 
+from spatialdata._io._utils import _resolve_zarr_store
+from spatialdata._io.exceptions import FormatVersionUnknownError, WritingToZarrV2DeprecationWarning
 from spatialdata._io.format import (
     CurrentTablesFormat,
     TablesFormats,
@@ -55,17 +60,67 @@ def write_table(
     name: str,
     group_type: str = "ngff:regions_table",
     element_format: Format = CurrentTablesFormat(),
+    convert_strings_to_categoricals: bool = False,
 ) -> None:
+    """
+    Write a table to a Zarr store.
+
+    Parameters
+    ----------
+    table
+        The table to write.
+    group
+        The table will be written into a subgroup of this group
+    name
+        The name of the subgroup of `group` to which table is to be written.
+    group_type
+        The type of the group.
+    element_format
+        The format to use for writing the table.
+    convert_strings_to_categoricals
+        If True, convert string columns to categoricals before writing.
+        Note that this will have a side effect of modifying dtypes of the input table in place.
+    """
+    if element_format.zarr_format == 2:
+        warnings.warn(
+            message=WritingToZarrV2DeprecationWarning.message, category=WritingToZarrV2DeprecationWarning, stacklevel=2
+        )
+
     if TableModel.ATTRS_KEY in table.uns:
         region, region_key, instance_key = get_table_keys(table)
         TableModel.validate(table)
     else:
         region, region_key, instance_key = (None, None, None)
 
-    write_adata(group, name, table)
-    tables_group = group[name]
-    tables_group.attrs["spatialdata-encoding-type"] = group_type
-    tables_group.attrs["region"] = region
-    tables_group.attrs["region_key"] = region_key
-    tables_group.attrs["instance_key"] = instance_key
-    tables_group.attrs["version"] = element_format.spatialdata_format_version
+    # Ensure the table group exists
+    table_group = group.require_group(name=name)
+
+    if element_format not in TablesFormats.values():
+        raise FormatVersionUnknownError(element_type="table", version_encountered=element_format)
+
+    if element_format.zarr_format == 3 and Version(version("anndata")) >= Version("0.13"):
+        # `write_zarr` in anndata v0.13 and above can only write to zarr v3
+        # solution of passing resolved store directly roughly based on:
+        # https://github.com/scverse/anndata/issues/1548#issuecomment-2199801855
+
+        # resolve the store from the group
+        resolved_store = _resolve_zarr_store(table_group)
+
+        # Write the table to the path of the table group
+        table.write_zarr(
+            store=resolved_store,
+            consolidate_metadata=False,
+            convert_strings_to_categoricals=convert_strings_to_categoricals,
+        )
+
+    else:
+        if convert_strings_to_categoricals:
+            table.strings_to_categoricals()
+
+        write_adata(group, name, table)
+
+    table_group.attrs["spatialdata-encoding-type"] = group_type
+    table_group.attrs["region"] = region
+    table_group.attrs["region_key"] = region_key
+    table_group.attrs["instance_key"] = instance_key
+    table_group.attrs["version"] = element_format.spatialdata_format_version
