8963e0469982a0aa74049f85ee9ef892c1c9d290
Tomaz Vieira <tom.fogaca@gmail.com>
2026-09-09 13:15:19 +0000

    Moves AxisParsingException to exceptions.py

--- src/spatialdata/_core/transformation_manager/exceptions.py
+++ src/spatialdata/_core/transformation_manager/exceptions.py
@@ -285,3 +285,7 @@ class UnmappedAxisError(Exception):
         self.axis = axis
         self.cs = cs
         super().__init__(f"Axis {axis.name} from coordinate system {cs.name} is not mapped to anything")
+
+
+class AxisParsingException(Exception):
+    pass
